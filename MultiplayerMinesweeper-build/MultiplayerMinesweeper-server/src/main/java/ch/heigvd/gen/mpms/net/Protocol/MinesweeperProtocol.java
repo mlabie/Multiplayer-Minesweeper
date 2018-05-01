a3dc446fdd0a1fa0ecaae97be7aa0dea903df562
Basler Corentin <corentin.basler@heig-vd.ch>
2018-05-01 17:44:26 +0200

    Add reply on the protocol

--- MultiplayerMinesweeper-build/MultiplayerMinesweeper-server/src/main/java/ch/heigvd/gen/mpms/net/Protocol/MinesweeperProtocol.java
+++ MultiplayerMinesweeper-build/MultiplayerMinesweeper-server/src/main/java/ch/heigvd/gen/mpms/net/Protocol/MinesweeperProtocol.java
@@ -10,6 +10,7 @@ public class MinesweeperProtocol {
 
     public final static String VERSION   = "1.0";
     public final static int DEFAULT_PORT = 1001;
+    public final static String EOL = "\r\n";
 
     // - - - - - - - - - - -          Status          - - - - - - - - - - - //
     public final static String STATUS_220 = "220";
@@ -39,8 +40,39 @@ public class MinesweeperProtocol {
     public final static String CMD_QUIT_GAME            = "QUIT GAME";
 
     // - - - - - - - - - - -          Answer         - - - - - - - - - - - //
-
-    //TODO:
-
+    public final static String REPLY_OK                         = "OK";
+    public final static String REPLY_ACTION_DENIED              = "ACTION DENIED";
+    public final static String REPLY_LOBBY_CREATE               = "LOBBY CREATE";
+    public final static String REPLY_LOBBY_NAME_NOT_AVAIABLE    = "LOBBY NAME NOT AVAILABLE";
+    public final static String REPLY_NO_LOBBY_CREATED           = "NO LOBBY CREATED";
+    public final static String REPLY_LOBBY_OPENED               = "LOBBY OPENED";
+    public final static String REPLY_LOBBY_CLOSED               = "LOBBY CLOSED";
+    public final static String REPLY_LOBBY_FULL                 = "LOBBY FULL";
+    public final static String REPLY_LOBBY_NOT_FOUND            = "LOBBY NOT FOUND";
+    public final static String REPLY_PLAYER_NAME_NOT_AVAIBALE   = "PLAYER NAME NOT AVAIABLE";
+    public final static String REPLY_LOBBY_JOINED_BY            = "LOBBY JOINED BY";
+    public final static String REPLY_NO_LOBBY_JOINED            = "NO LOBBY JOINED";
+    public final static String REPLY_LOBBY_LEFT_BY              = "LOBBY LEFT BY";
+    public final static String REPLY_PLAYER_NOT_FOUND           = "PLAYER NOT FOUND";
+    public final static String REPLY_YOU_HAVE_BEEN_EXPELED      = "YOU HAVE BEEN EXPELED";
+    public final static String REPLY_MODE_NOT_FOUND             = "MODE NOT FOUND";
+    public final static String REPLY_SCORE_MODE_IS              = "SCORE MODE IS";
+    public final static String REPLY_MINE_AMOUNT_NOT_ALLOWED    = "MINE AMOUNT NOT ALLOWED";
+    public final static String REPLY_MINE_AMOUNT_IS             = "MINE AMOUNT IS";
+    public final static String REPLY_SIZE_NOT_ALLOWED           = "SIZE IS NOT ALLOWED";
+    public final static String REPLY_SIZE_IS                    = "SIZE IS";
+    public final static String REPLY_PLAYER_AMOUNT_NOT_ALLOWED  = "PLAYER AMOUNT NOT ALLOWED";
+    public final static String REPLY_PLAYER_AMOUNT_IS           = "PLAYER AMOUNT IS";
+    public final static String REPLY_BONUS_MALUS_ENABLED        = "BONUS-MALUS ENABLED";
+    public final static String REPLY_BONUS_MALUS_DISABLED       = "BONUS-MALUS DISABLED";
+    public final static String REPLY_GAME_STARTED               = "GAME STARTED";
+    public final static String REPLY_SQUARE_ALREADY_SWEPT       = "SQUARE ALREADY SWEPT";
+    public final static String REPLY_YOU_HAVE_ALREADY_LOST      = "YOU HAVE ALREADY LOST";
+    public final static String REPLY_SQUARE_NOT_FOUND           = "SQUARE NOT FOUND";
+    public final static String REPLY_SQUARE_SWEPT               = "SQUARE SWEPT";
+    public final static String REPLY_PLAYER_SCORE               = "PLAYER SCORE";
+    public final static String REPLY_NOT_IN_A_GAME              = "NOT IN A GAME";
+    public final static String REPLY_PLAYER_LEFT                = "PLAYER LEFT";
+    public final static String REPLY_GAME_FINISHED              = "GAME FINISHED";
 
 }
