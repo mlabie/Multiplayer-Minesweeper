bfa8923f450aa6f593b15ac51c462d400fee3fb7
antonio.cusanelli <antonio.cusanelli@heig-vd.ch>
2018-05-02 19:04:53 +0200

    début client

--- MultiplayerMinesweeper-build/MultiplayerMinesweeper-client/src/main/java/ch/heigvd/gen/mpms/net/client/IMinesweeperClient.java
+++ MultiplayerMinesweeper-build/MultiplayerMinesweeper-client/src/main/java/ch/heigvd/gen/mpms/net/client/IMinesweeperClient.java
@@ -1,4 +1,20 @@
 package ch.heigvd.gen.mpms.net.client;
 
 public interface IMinesweeperClient {
+
+	void connect(String addressServer, int port);
+
+	void disconnect();
+
+	boolean isConnected();
+
+	void createLobby();
+
+	void joinLobby(String playerName, String lobbyName);
+
+	void quitLobby();
+
+	void sweep(int x, int y);
+
+	void quitGame();
 }
