2dee41816c6476f8df57832cfd52ba64dae60607
marc.labie <marc.labie@heig-vd.ch>
2018-05-04 01:56:52 +0200

    Implémentation de la fonction main du serveur pour lancer le serveur. Première implémentation de la fonction managecommand de servantWorker pour gérer les requêtes du client.

--- MultiplayerMinesweeper-build/MultiplayerMinesweeper-server/src/main/java/ch/heigvd/gen/mpms/MinesweeperServer.java
+++ MultiplayerMinesweeper-build/MultiplayerMinesweeper-server/src/main/java/ch/heigvd/gen/mpms/MinesweeperServer.java
@@ -1,7 +1,31 @@
 package ch.heigvd.gen.mpms;
 
+import ch.heigvd.gen.mpms.net.Protocol.MinesweeperProtocol;
+import ch.heigvd.gen.mpms.net.server.ReceptionistWorker;
+
 public class MinesweeperServer {
     public static void main(String args[]){
-        System.out.println("Hello Server !");
+
+        int port;
+
+        if(args.length == 0) {
+
+            port = MinesweeperProtocol.DEFAULT_PORT;
+
+        } else if(args.length == 1) {
+
+            port = Integer.parseInt(args[0]);
+
+            if(port > 6000 || port < 1000){
+                System.out.println("The port : " + port + " is not an available one.");
+                return;
+            }
+
+        } else {
+            System.out.println("Too many arguments.");
+            return;
+        }
+
+        new Thread(new ReceptionistWorker(port)).start();
     }
 }
--- MultiplayerMinesweeper-build/MultiplayerMinesweeper-server/src/main/java/ch/heigvd/gen/mpms/net/server/ServantWorker.java
+++ MultiplayerMinesweeper-build/MultiplayerMinesweeper-server/src/main/java/ch/heigvd/gen/mpms/net/server/ServantWorker.java
@@ -1,5 +1,7 @@
 package ch.heigvd.gen.mpms.net.server;
 
+import ch.heigvd.gen.mpms.net.Protocol.MinesweeperProtocol;
+
 import java.io.*;
 import java.net.Socket;
 import java.util.logging.Level;
@@ -30,16 +32,41 @@ public class ServantWorker implements Runnable{
     }
 
     public void run() {
-        String line;
-        boolean shouldRun = true;
+
+        String request;         // The command send by the client
+        String answer;          // The answer from the server
+        boolean shouldRun;
+        int manageRes;
+
+
+        shouldRun = true;
+        answer    = "Welcome to the Multiplayer MineSweeper game !";
+
+        LOG.info(answer);
+        print(answer);
 
         try {
-            LOG.info("Reading until client sends BYE or closes the connection...");
-            while ((shouldRun) && (line = br.readLine()) != null) {
+
+            LOG.info("Reading until client sends QUIT or closes the connection...");
+
+            while ((shouldRun) && (request = br.readLine()) != null) {
+
+                request = request.toUpperCase();
+
+                LOG.info(request);
+
+                manageRes = manageCommand(request);
+
+                // Depending on the result of the manageCommand, the connection will be closed or not
+                // (-1 : close connection, 0 : continue)
+                if(manageRes == -1){
+                    shouldRun = false;
+                }
 
             }
 
             LOG.info("Cleaning up resources...");
+
             br.close();
             pw.close();
             clientSocket.close();
@@ -64,5 +91,159 @@ public class ServantWorker implements Runnable{
             }
             LOG.log(Level.SEVERE, e.getMessage(), e);
         }
+    }
+
+    /**
+     *
+     * @param request
+     */
+    private int manageCommand(String request){
+
+        String answer;
+        String command;
+
+        int iter_command;
+
+        for(iter_command = 0; iter_command < MinesweeperProtocol.SUPPORTED_COMMANDS.length; iter_command ++)
+            if (request.startsWith(MinesweeperProtocol.SUPPORTED_COMMANDS[iter_command]))
+                break;
+
+        command = MinesweeperProtocol.SUPPORTED_COMMANDS[iter_command];
+
+        switch (command){
+
+            case MinesweeperProtocol.CMD_CLOSE_LOBBY:
+                answer = MinesweeperProtocol.STATUS_650 + " the command \"" + MinesweeperProtocol.CMD_CLOSE_LOBBY +
+                         "\" has not been implemented yet.";
+                this.print(answer);
+                LOG.log(Level.INFO, answer);
+                break;
+
+            case MinesweeperProtocol.CMD_CREATE_LOBBY:
+                answer = MinesweeperProtocol.STATUS_650 + " the command \"" + MinesweeperProtocol.CMD_CREATE_LOBBY +
+                        "\" has not been implemented yet.";
+                this.print(answer);
+                LOG.log(Level.INFO, answer);
+                break;
+
+            case MinesweeperProtocol.CMD_DISABLE_BONUS_MALUS:
+                answer = MinesweeperProtocol.STATUS_650 + " the command \"" + MinesweeperProtocol.CMD_DISABLE_BONUS_MALUS +
+                        "\" has not been implemented yet.";
+                this.print(answer);
+                LOG.log(Level.INFO, answer);
+                break;
+
+            case MinesweeperProtocol.CMD_DISCONNECT:
+                answer = "Thank's for playing ! see you soon.";
+                this.print(answer);
+                LOG.log(Level.INFO, answer);
+                return -1;
+
+            case MinesweeperProtocol.CMD_ENABLE_BONUS_MALUS:
+                answer = MinesweeperProtocol.STATUS_650 + " the command \"" + MinesweeperProtocol.CMD_ENABLE_BONUS_MALUS +
+                        "\" has not been implemented yet.";
+                this.print(answer);
+                LOG.log(Level.INFO, answer);
+                break;
+
+            case MinesweeperProtocol.CMD_EXPEL_LOBBY:
+                answer = MinesweeperProtocol.STATUS_650 + " the command \"" + MinesweeperProtocol.CMD_EXPEL_LOBBY +
+                        "\" has not been implemented yet.";
+                this.print(answer);
+                LOG.log(Level.INFO, answer);
+                break;
+
+
+            case MinesweeperProtocol.CMD_JOIN_LOBBY:
+                answer = MinesweeperProtocol.STATUS_650 + " the command \"" + MinesweeperProtocol.CMD_JOIN_LOBBY +
+                        "\" has not been implemented yet.";
+                this.print(answer);
+                LOG.log(Level.INFO, answer);
+                break;
+
+            case MinesweeperProtocol.CMD_OPEN_LOBBY:
+                answer = MinesweeperProtocol.STATUS_650 + " the command \"" + MinesweeperProtocol.CMD_OPEN_LOBBY +
+                        "\" has not been implemented yet.";
+                this.print(answer);
+                LOG.log(Level.INFO, answer);
+                break;
+
+            case MinesweeperProtocol.CMD_QUIT_GAME:
+                answer = MinesweeperProtocol.STATUS_650 + " the command \"" + MinesweeperProtocol.CMD_QUIT_GAME +
+                        "\" has not been implemented yet.";
+                this.print(answer);
+                LOG.log(Level.INFO, answer);
+                break;
+
+            case MinesweeperProtocol.CMD_QUIT_LOBBY:
+                answer = MinesweeperProtocol.STATUS_650 + " the command \"" + MinesweeperProtocol.CMD_QUIT_LOBBY +
+                        "\" has not been implemented yet.";
+                this.print(answer);
+                LOG.log(Level.INFO, answer);
+                break;
+
+
+            case MinesweeperProtocol.CMD_SET_MINE_AMOUNT:
+                answer = MinesweeperProtocol.STATUS_650 + " the command \"" + MinesweeperProtocol.CMD_SET_MINE_AMOUNT +
+                        "\" has not been implemented yet.";
+                this.print(answer);
+                LOG.log(Level.INFO, answer);
+                break;
+
+
+            case MinesweeperProtocol.CMD_SET_PLAYER_AMOUNT:
+                answer = MinesweeperProtocol.STATUS_650 + " the command \"" + MinesweeperProtocol.CMD_SET_PLAYER_AMOUNT +
+                        "\" has not been implemented yet.";
+                this.print(answer);
+                LOG.log(Level.INFO, answer);
+                break;
+
+            case MinesweeperProtocol.CMD_SET_SCORE_MODE:
+                answer = MinesweeperProtocol.STATUS_650 + " the command \"" + MinesweeperProtocol.CMD_SET_SCORE_MODE +
+                        "\" has not been implemented yet.";
+                this.print(answer);
+                LOG.log(Level.INFO, answer);
+                break;
+
+
+            case MinesweeperProtocol.CMD_SET_SIZE:
+                answer = MinesweeperProtocol.STATUS_650 + " the command \"" + MinesweeperProtocol.CMD_SET_SIZE +
+                        "\" has not been implemented yet.";
+                this.print(answer);
+                LOG.log(Level.INFO, answer);
+                break;
+
+
+            case MinesweeperProtocol.CMD_START_GAME:
+                answer = MinesweeperProtocol.STATUS_650 + " the command \"" + MinesweeperProtocol.CMD_START_GAME +
+                        "\" has not been implemented yet.";
+                this.print(answer);
+                LOG.log(Level.INFO, answer);
+                break;
+
+            case MinesweeperProtocol.CMD_SWEEP:
+                answer = MinesweeperProtocol.STATUS_650 + " the command \"" + MinesweeperProtocol.CMD_SWEEP +
+                        "\" has not been implemented yet.";
+                this.print(answer);
+                LOG.log(Level.INFO, answer);
+                break;
+
+
+            default:
+                answer = MinesweeperProtocol.STATUS_750 + " " + MinesweeperProtocol.REPLY_UNKNOWN_COMMAND;
+                this.print(answer);
+                LOG.log(Level.INFO, answer);
+                break;
+        }
+
+        return 0;
+    }
+
+    /**
+     * Function that sends an answer to the client, using the CARRIAGE_RETURN character.
+     * @param answer    : The answer to send
+     */
+    private void print(String answer){
+        pw.println(answer + MinesweeperProtocol.CARRIAGE_RETURN);
     }
 }
