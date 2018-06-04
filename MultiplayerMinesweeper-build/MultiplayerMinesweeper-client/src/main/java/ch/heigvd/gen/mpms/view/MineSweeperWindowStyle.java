1dd3479255947a8b5319f88d1c341c17cf4558e2
marc.labie <marc.labie@heig-vd.ch>
2018-06-04 11:08:10 +0200

    Updating the MineSweeperWindow style for the player

--- MultiplayerMinesweeper-build/MultiplayerMinesweeper-client/src/main/java/ch/heigvd/gen/mpms/view/MineSweeperWindowStyle.java
+++ MultiplayerMinesweeper-build/MultiplayerMinesweeper-client/src/main/java/ch/heigvd/gen/mpms/view/MineSweeperWindowStyle.java
@@ -1,4 +1,44 @@
 package ch.heigvd.gen.mpms.view;
 
-public interface MineSweeperWindowStyle {
+import java.util.HashMap;
+import java.util.Map;
+
+import javafx.geometry.Insets;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
+
+
+public class MineSweeperWindowStyle {
+
+    private static Background b1 = new Background(new BackgroundFill(Color.SKYBLUE, CornerRadii.EMPTY, Insets.EMPTY));
+    private static Background b2 = new Background(new BackgroundFill(Color.LIGHTGREEN, CornerRadii.EMPTY, Insets.EMPTY));
+    private static Background b3 = new Background(new BackgroundFill(Color.LIGHTSALMON, CornerRadii.EMPTY, Insets.EMPTY));
+    private static Background b4 = new Background(new BackgroundFill(Color.KHAKI, CornerRadii.EMPTY, Insets.EMPTY));
+
+    /***************        Colours       ***************/
+
+    public final static Map<Integer, Color> VALUE_COLOUR = new HashMap<Integer, Color>();
+    static {
+        VALUE_COLOUR.put(0, Color.WHITE);
+        VALUE_COLOUR.put(1, Color.BLUE);
+        VALUE_COLOUR.put(2, Color.GREEN);
+        VALUE_COLOUR.put(3, Color.RED);
+        VALUE_COLOUR.put(4, Color.DARKBLUE);
+        VALUE_COLOUR.put(5, Color.DARKGREEN);
+        VALUE_COLOUR.put(6, Color.DARKRED);
+        VALUE_COLOUR.put(7, Color.DARKGRAY);
+        VALUE_COLOUR.put(8, Color.LIGHTGRAY);
+    }
+
+    public final static Map<Integer, Background> PLAYER_COLOUR = new HashMap<Integer, Background>();
+    static {
+        PLAYER_COLOUR.put(1, b1);
+        PLAYER_COLOUR.put(2, b2);
+        PLAYER_COLOUR.put(3, b3);
+        PLAYER_COLOUR.put(4, b4);
+    }
+
+
 }
